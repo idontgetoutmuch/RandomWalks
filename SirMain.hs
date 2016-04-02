73410c5deab0fa54c264d5482cec3c2cde0fd023
Dominic Steinitz <dominic@steinitz.org>
2016-04-02 16:58:53 +0100

    Charts for blog article.

---
+++ SirMain.hs
@@ -0,0 +1,83 @@
+{-# OPTIONS_GHC -Wall                      #-}
+{-# OPTIONS_GHC -fno-warn-name-shadowing   #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults    #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind   #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods  #-}
+{-# OPTIONS_GHC -fno-warn-orphans          #-}
+
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Prelude hiding ( render, Renderable )
+import Diagrams.Backend.CmdLine
+
+import System.IO.Unsafe
+
+import OdeExample
+import Numeric.LinearAlgebra
+
+
+denv :: DEnv Double
+denv = unsafePerformIO $ defaultEnv vectorAlignmentFns 600 500
+
+diagSirGen :: String ->
+                 [(Double, Double)] ->
+                 [(Double, Double)] ->
+                 [(Double, Double)] ->
+                 Diagram Cairo
+diagSirGen t l xs es =
+  fst $ runBackend denv (render (chartSirGen t l xs es) (600, 500))
+
+chartSirGen :: String ->
+              [(Double, Double)] ->
+              [(Double, Double)] ->
+              [(Double, Double)] ->
+              Renderable ()
+chartSirGen title acts obs ests = toRenderable layout
+  where
+
+    actuals = plot_lines_values .~ [acts]
+            $ plot_lines_style  . line_color .~ opaque red
+            $ plot_lines_title .~ "Susceptible"
+            $ plot_lines_style  . line_width .~ 2.0
+            $ def
+
+    measurements = plot_lines_values .~ [obs]
+                 $ plot_lines_style  . line_color .~ opaque blue
+            $ plot_lines_style  . line_width .~ 2.0
+                 $ plot_lines_title .~ "Infected"
+                 $ def
+
+    estimas = plot_lines_values .~ [ests]
+            $ plot_lines_style  . line_color .~ opaque black
+            $ plot_lines_title .~ "Recovered"
+            $ plot_lines_style  . line_width .~ 2.0
+            $ def
+
+    layout = layout_title .~ title
+           $ layout_plots .~ [toPlot actuals, toPlot measurements, toPlot estimas]
+           $ layout_y_axis . laxis_title .~ "Pupil Numbers"
+           $ layout_y_axis . laxis_override .~ axisGridHide
+           $ layout_x_axis . laxis_title .~ "Time"
+           $ layout_x_axis . laxis_override .~ axisGridHide
+           $ def
+
+
+displayHeader :: FilePath -> Diagram B -> IO ()
+displayHeader fn =
+  mainRender ( DiagramOpts (Just 900) (Just 700) fn
+             , DiagramLoopOpts False Nothing 0
+             )
+
+main :: IO ()
+main = do
+  let xs = map toList $ toRows $ tr sol
+      ss = xs!!0
+      is = xs!!1
+      rs = xs!!2
+  displayHeader "diagrams/Sir.png"
+                (diagSirGen "Influenza Outbreak"
+                               (zip [0,1..] ss)
+                               (zip [0,1..] is)
+                               (zip [0,1..] rs))
+  putStrLn "Hello"
